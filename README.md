982f2a4f9513aab7fe0efd43f3f1ae64ef971060
Eric D. White <eric@ericwhite.ca>
2015-06-20 12:31:45 +0100

    Allow the proxy to forward headers from originating request.  Including: Authorization for use with APIs that require basic auth.

--- README.md
+++ README.md
@@ -1,14 +1,32 @@
 crossorigin.me
 ==============
 
-A CORS proxy replacement. Based partly on https://github.com/bmpvieira/simple-corsproxy.
+A CORS proxy replacement. Based partly on https://github.com/technoboy10/crossorigin.me 
 
 ## What?
 crossorigin.me is a CORS proxy. It lets developers (like you!) access resources from other sites that don't have CORS enabled on their server.
 
-## Why?
-One of the more well known CORS proxies, corsproxy.com, recently disappeared. Quite a few things relied on that site, so I decided to set a new proxy up. 
+Installation
+------------
+To run this proxy you will need node js.
 
-## Where?
+    $ git clone https://github.com/ericdwhite/crossorigin.me
+    $ cd crossorigin.me
+    $ npm install
 
-[Here](http://crossorigin.me).
+Running
+-------
+In the default case you can just run with npm.
+
+    $ npm start
+
+There are additional parameters that can be set via the environment.
+
+    # PORT - (default 8080)
+    # AC_ALLOW_HEADERS - a comma deliminted list of 'Access-Control-Allow-Headers' (default Content-Type)
+
+The AC_ALLOW_HEADERS allows the CORS proxy to pass through additional headers.  For example to support JSON with basic auth
+you can use:
+
+    $ AC_ALLOW_HEADERS="Authorization, Content-Type" npm start
+
--- index.js
+++ index.js
@@ -7,12 +7,15 @@ var index = fs.readFileSync('index.html');
 var favicon = fs.readFileSync('favicon.ico');
 
 var port = process.env.PORT || 8080
+var AC_ALLOW_HEADERS = process.env.AC_ALLOW_HEADERS || 'Content-Type' 
+
+console.log('Listenting on PORT: ' + port)
+console.log('Access-Control-Allow-Headers: ' + AC_ALLOW_HEADERS)
 
 var server = http.createServer(function (req, res) {
   var d = domain.create();
   d.on('error', function (e){
     console.log('ERROR', e.stack);
-
     res.statusCode = 500;
     res.end('Error: ' +  ((e instanceof TypeError) ? "make sure your URL is correct" : String(e)));
   });
@@ -25,7 +28,7 @@ var server = http.createServer(function (req, res) {
   });
 
 }).listen(port);
-
+console.log('Started\n')
 
 function handler(req, res) {
   console.log(req.url);
@@ -45,21 +48,38 @@ function handler(req, res) {
     res.write(favicon);
     res.end()
     default:
-      if (req.url.indexOf('vivastreet') > -1){
-      res.end('tempbanned');
-    } else {
       try {
-        res.setTimeout(25000);
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Credentials', false);
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-        request(req.url.slice(1), {encoding: null}, function(error, response, body) {
+      res.setTimeout(25000);
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+      res.setHeader('Access-Control-Allow-Credentials', false);
+      res.setHeader('Access-Control-Allow-Headers', AC_ALLOW_HEADERS);
+      if (req.method.toUpperCase() === 'OPTIONS') {
+        res.statusCode = 200
+        res.end()
+      } else {
+        // Proxy to the server
+        var proxiedURL = req.url.slice(1)
+        console.log('Proxy to: ' + req.method + ' ' + proxiedURL)
+
+        var forwardHeaders = req.headers
+        delete forwardHeaders.origin
+        delete forwardHeaders.host
+        //console.log(forwardHeaders)
+
+        var r = request(proxiedURL, {encoding: null, headers: forwardHeaders}, function(error, response, body) {
+          //console.log('Proxied headers:')
+          //console.log(response.request.headers)
+          //console.log(response.statusCode)
+          res.statusCode = response.statusCode
           res.write(body)
           res.end()
         })
-      } catch (e) {
-        res.end('Error: ' +  ((e instanceof TypeError) ? "make sure your URL is correct" : String(e)));
       }
+
+    } catch (e) {
+      console.log('ERROR', e.stack);
+      res.end('Error: ' +  ((e instanceof TypeError) ? "make sure your URL is correct" : String(e)));
     }
   }
 }
